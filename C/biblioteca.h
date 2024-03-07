b35a9c004e7bb654ea08d07c46ac611342823b46
Otavio <taviussm@hotmail.com>
2024-03-07 20:09:12 -0300

    Atualizações na biblioteca.h

--- C/biblioteca.h
+++ C/biblioteca.h
@@ -1,46 +1,15 @@
-#include <stdio.h>
-#ifndef DIVISIVEL_POR_5_E_11_H
-#define DIVISIVEL_POR_5_E_11_H
+#ifndef BIBLIOTECA_H
+#define BIBLIOTECA_H
+#include "biblioteca.c"
 
-int divisivel_por_5_e_11(int n) {
-    if(n % 5 == 0 && n % 11 == 0) {
-        return 0;
-    } 
-    else {
-        return 1;
-    }
+int divisivel_por_5_e_11(int n);
 
-}
+int soma_entre_1_e_n(int n);
 
-int soma_entre_1_e_n(int n) {
-    int soma = 0;
-    for (int i = 1; i <= n; i++) {
-        soma += i;
-    }
-    return soma;
-}
+int maior_valor_vetor(int *vetor, int n);
 
-int maior_valor_vetor(int *vetor, int n){
-    int maior = vetor[0];
-    for(int i = 1; i < n; i++){
-        if(vetor[i] > maior){
-            maior = vetor[i];
-        }
-    }
-    return maior;
-}
+void imprime_naturais(int n);
 
-void imprime_naturais(int n){
-    if(n > 0){
-        imprime_naturais(n - 1);
-        printf("%d\n", n);
-    }
-}
-
-void inverte(int *a, int *b){
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
+void inverte(int *a, int *b);
 
 #endif

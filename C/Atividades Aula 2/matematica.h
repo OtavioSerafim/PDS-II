2db22b3887604f724b44b39597430742699c84b3
Otavio <taviussm@hotmail.com>
2024-03-11 14:44:38 -0300

    Adição de parametros e returns nos arquivos de cabeçalho

--- C/Atividades Aula 2/matematica.h
+++ C/Atividades Aula 2/matematica.h
@@ -1,13 +1,25 @@
 #ifndef libmatematica
 #define libmatematica
 
-// Calcula o seno de um ângulo em radianos.
+/**
+ * @brief Calcula o seno de um ângulo em radianos.
+ * @param angulo_radianos O ângulo em radianos.
+ * @return O seno do ângulo.
+ */
 float calcular_seno(float angulo_radianos);
 
-// Calcula o cosseno de um ângulo em radianos.
+/**
+ * @brief Calcula o cosseno de um ângulo em radianos.
+ * @param angulo_radianos O ângulo em radianos.
+ * @return O cosseno do ângulo.
+ */
 float calcular_cosseno(float angulo_radianos);
 
-// Calcula a tangente de um ângulo em radianos.
+/**
+ * @brief Calcula a tangente de um ângulo em radianos.
+ * @param angulo_radianos O ângulo em radianos.
+ * @return A tangente do ângulo.
+ */
 float calcular_tangente(float angulo_radianos);
 
 #endif
--- C/Atividades aula 1/biblioteca.h
+++ C/Atividades aula 1/biblioteca.h
@@ -1,20 +1,39 @@
 #ifndef BIBLIOTECA_H
 #define BIBLIOTECA_H
 
-// Verifica se um número é divisível por 5 e 11.
-// Retorna 0 se o número for divisível por ambos, caso contrário retorna 1.
+/**
+ * @brief Verifica se um número é divisível por 5 e 11.
+ * @param n O número a ser verificado.
+ * @return Retorna 0 se o número for divisível por ambos, caso contrário retorna 1.
+ */
 int divisivel_por_5_e_11(int n);
 
-// Calcula a soma de todos os números entre 1 e n.
+/**
+ * @brief Calcula a soma de todos os números entre 1 e n.
+ * @param n O número até onde a soma será calculada.
+ * @return A soma de todos os números entre 1 e n.
+ */
 int soma_entre_1_e_n(int n);
 
-// Encontra e retorna o maior valor em um vetor de inteiros.
+/**
+ * @brief Encontra e retorna o maior valor em um vetor de inteiros.
+ * @param vetor O vetor de inteiros.
+ * @param n O número de elementos no vetor.
+ * @return O maior valor encontrado no vetor.
+ */
 int maior_valor_vetor(int *vetor, int n);
 
-// Imprime todos os números naturais até n em ordem crescente.
+/**
+ * @brief Imprime todos os números naturais até n em ordem crescente.
+ * @param n O número até onde os números naturais serão impressos.
+ */
 void imprime_naturais(int n);
 
-// Inverte os valores de duas variáveis.
+/**
+ * @brief Inverte os valores de duas variáveis.
+ * @param a Ponteiro para a primeira variável.
+ * @param b Ponteiro para a segunda variável.
+ */
 void inverte(int *a, int *b);
 
 #endif
